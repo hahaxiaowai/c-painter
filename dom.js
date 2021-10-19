10f21c5c461510209f82a409d398d677bbb38ba9
hahaxiaowai <1062080110@qq.com>
2021-10-19 16:31:24 +0800

    feat: init

---
+++ dom.js
@@ -0,0 +1,7 @@
+ export function initCanvas(elementId){
+   const parentDiv = document.getElementById(elementId)
+   if(!parentDiv){
+     console.error('请传入正确的id')
+     return
+   }
+ }
---
+++ index.js
@@ -0,0 +1,11 @@
+import { initCanvas } from './dom';
+class Painter{
+  constructor(elementId){
+    // 初始化Dom元素
+    const res = initCanvas(elementId);
+    // 初始化变量池
+    // initVariable()
+    // 初始化事件
+    // initEvent();
+  }
+}
