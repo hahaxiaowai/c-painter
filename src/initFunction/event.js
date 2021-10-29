ce264343f07b2d2a050072d28504bbf0203d6561
hahaxiaowai <1062080110@qq.com>
2021-10-29 17:58:20 +0800

    feat: 保存

--- src/initFunction/event.js
+++ src/initFunction/event.js
@@ -1,13 +1,13 @@
-export function initEvent(doms, state) {
+export function initEvent(doms, state,storage) {
   // PC
   doms.canvas_draw.onmousedown = (e) => mouseDown(e, doms.ctx_draw, state);
   doms.canvas_draw.onmousemove = (e) => mouseMove(e, doms.ctx_draw, state);
-  doms.canvas_draw.onmouseup = (e) => mouseUp(e, doms.ctx_draw, state);
-  doms.canvas_draw.onmouseleave = (e) => mouseUp(e, doms.ctx_draw, state);
+  doms.canvas_draw.onmouseup = (e) => mouseUp(e, doms.ctx_draw, state,storage);
+  doms.canvas_draw.onmouseleave = (e) => mouseUp(e, doms.ctx_draw, state,storage);
   // 移动端
   doms.canvas_draw.ontouchstart = (e) => mouseDown(e, doms.ctx_draw, state);
   doms.canvas_draw.ontouchmove = (e) => mouseMove(e, doms.ctx_draw, state);
-  doms.canvas_draw.ontouchend = (e) => mouseUp(e, doms.ctx_draw, state);
+  doms.canvas_draw.ontouchend = (e) => mouseUp(e, doms.ctx_draw, state,storage);
 }
 
 function mouseDown(e, ctx_draw, state) {
@@ -27,7 +27,12 @@ function mouseMove(e, ctx_draw, state) {
     ctx_draw.stroke();
   }
 }
-function mouseUp(e, ctx_draw, state) {
+function mouseUp(e, ctx_draw, state,storage) {
   ctx_draw.closePath();
   state.boolDraw = false;
+  storage.add()
 }
+function toBase64(ctx_draw){
+  
+}
+
