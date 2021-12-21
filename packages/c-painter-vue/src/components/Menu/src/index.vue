ceb794df2304460216014cf8fb083146a44745fd
hahaxiaowai <1062080110@qq.com>
2021-12-21 13:39:49 +0800

    feat: menu

--- packages/c-painter-vue/src/components/Menu/src/index.vue
+++ packages/c-painter-vue/src/components/Menu/src/index.vue
@@ -1,5 +1,17 @@
 <template>
-    <button>123</button>
+  <div class="flex">
+        <a-dropdown-button @click="handleButtonClick">
+      
+      <template #overlay>
+        
+      </template>
+    </a-dropdown-button>
+    <select name="" id="">
+      <option value="freePen">自由绘制</option>
+      <option value="eraser">橡皮</option>
+    </select>
+    
+  </div>
 </template>
 
 <script setup>
--- packages/c-painter-vue/src/main.js
+++ packages/c-painter-vue/src/main.js
@@ -1,7 +1,8 @@
 import { createApp } from 'vue'
+import { DropdownButton } from 'ant-design-vue';
 import App from './App.vue'
 import Menu from './components/Menu'
-console.log(Menu)
 createApp(App)
+.use(DropdownButton)
 .use(Menu).mount('#app')
 
